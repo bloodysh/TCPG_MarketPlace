f8344dc725f474daa23704241995c998347cde33
bloodysh <anas.benmsatef@lycee-faure.fr>
2025-03-15 15:47:08 +0100

    save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { CardsTableComponent } from "./cards-table/cards-table.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, CardsTableComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, CardsTableComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   standalone: true
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { CardsTableComponent } from './cards-table/cards-table.component';
 import { HomeComponent } from './home/home.component';
+import { CollectionComponent } from './collection/collection.component';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path : 'home', component: HomeComponent },
-    { path : 'ApexGen', component: CardsTableComponent },
+    { path : 'apexGen', component: CardsTableComponent },
+    { path : 'collection', component: CollectionComponent },
 ];
