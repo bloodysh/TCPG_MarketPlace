c12a9c7fc4d363a10aec3ee6fe6fdbbdca40a1fe
Leon <me@leonekmi.fr>
2025-03-18 21:52:48 +0100

    wip angular material

--- src/app/collection/collection.component.ts
+++ src/app/collection/collection.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
+import {MatGridList, MatGridTile} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-collection',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, MatGridList, MatGridTile],
   templateUrl: './collection.component.html',
   styleUrls: ['./collection.component.css']
 })
